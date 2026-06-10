ffe4c7ec1b4ddf7aad1d901c18db7bccd128e40b
Felipe Piva <157409408+Felipepiv@users.noreply.github.com>
2026-06-10 20:19:20 -0300

    fazendo um GET para carregar a lista ao abrir a página

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-// Arquivo para código javascript
+conts API_URL = 'https://6a1f56a9b79eec0d6cf0a932.mockapi.io/api/v1/users'
 
+// carregar a lista ao abrir a página do sistema
 
+async function carregarMaterias (){
+  try {
+    conts res = await fetch(API_URL);
+    if (!res.ok) thorw new Error('Erro ao buscar dados.');
+    conts materiais = await res.json();
+  } cath (e) {
+    document.getElementById('lista-materias=is').innerHTML = ´<p class="status" style="color:red;"> ${e.message}</p>´;
+  }
+}
