b1e733292a34ede06a7fdd996ade87a2ebc57adf
Felipe Piva <157409408+Felipepiv@users.noreply.github.com>
2026-06-17 19:20:29 -0300

    Adicionando a função validarRetirada

--- script.js
+++ script.js
@@ -2,14 +2,34 @@ conts API_URL = 'https://6a1f56a9b79eec0d6cf0a932.mockapi.io/api/v1/users'
 
 // carregar a lista ao abrir a página do sistema
 
-async function carregarMaterias (){
-  try {
-    conts res = await fetch(API_URL);
-    if (!res.ok) thorw new Error('Erro ao buscar dados.');
-    conts materiais = await res.json();
-  } cath (e) {
-    document.getElementById('lista-materias=is').innerHTML = ´<p class="status" style="color:red;"> ${e.message}</p>´;
-  }
+async function carregarMateriais() {
+    try {
+        const res = await fetch(API_URL);
+
+        if (!res.ok) throw new Error('Erro ao buscar dados.');
+
+        const materiais = await res.json();
+
+        renderizarLista(materiais);
+
+    } catch (e) {
+
+        document.getElementById('lista-materiais').innerHTML =
+            `<p class="status" style="color:red;">⚠️ ${e.message}</p>`;
+    }
+}
+
+function validarRetirada(estoqueAtual, quantidadeRetirada) {
+
+    if (quantidadeRetirada <= 0) {
+        return false;
+    }
+
+    if (quantidadeRetirada > estoqueAtual) {
+        return false;
+    }
+
+    return true;
 }
 
 function renderizarLista(materiais) {
